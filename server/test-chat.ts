40b54f23ca02e91282f3a74cc14bdcd5d213a0b2
your-github-username <your-github-email>
2025-09-08 09:18:31 +0530

    feat: Add personal information, projects, and skills to portfolio

---
+++ server/test-chat.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+(async () => {
+  const res = await axios.post("http://localhost:3000/api/chat", {
+    message: "Who is Devicharan?",
+  });
+  console.log("Bot:", res.data.reply);
+})();
